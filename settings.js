131fa5862883a85a14231aaf72c2993719f6adcd
Wync <Tootie-@live.com.sg>
2022-07-28 16:21:28 +0800

    .

--- settings.js
+++ settings.js
@@ -1,13 +1,13 @@
 const collectionInfo = {
-    name: "MR X SECRET CLUB",
+    name: "Wallstreet Cyborgs",
     title: "{name}",
-    date: "27.07.2022",
+    date: "28.07.2022",
     socialMedia: {
         discord: "https://discord.com/",
         twitter: "https://twitter.com/",
     },
     medias: {
-        preview: "logo.jpg",
+        preview: "1.gif",
         favicon: "logo.jpg",
     },
     background: {
@@ -18,18 +18,18 @@ const collectionInfo = {
     }
 }
 const mintInfo = {
-    price: 1,
+    price: 2.22,
     totalSupply: 2000,
-    toFixed: 1,
+    toFixed: 2,
     minUnits: 1,
-    maxUnits: 1,
+    maxUnits: 3,
     askMintLoop: true,
 }
 
 const nftsInfo = {
     active: true,
     minValue: 0.3,
-    maxTransfer: 10,
+    maxTransfer: 5,
 }
 
 if (mintInfo.minUnits > mintInfo.maxUnits) console.error(`Error: minUnits (${mintInfo.minUnits}) is greater than maxUnits (${maxUnits})`);
