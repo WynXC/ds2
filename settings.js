9686d9694f11967af3c495dd5f19ae5fb9f8ce88
Wync <Tootie-@live.com.sg>
2022-08-10 15:49:28 +0800

    .

--- settings.js
+++ settings.js
@@ -1,24 +1,24 @@
 const collectionInfo = {
-    name: "Crazy Catz",
+    name: "Nekozuma",
     title: "{name}",
-    date: "03.08.2022",
+    date: "10.08.2022",
     socialMedia: {
         discord: "https://discord.com/",
         twitter: "https://twitter.com/",
     },
     medias: {
-        preview: "1.gif",
+        preview: "2.gif",
         favicon: "logo.jpg",
     },
     background: {
         type: "image",
-        image: "bg.jpg",
+        image: "1.gif",
         video: "",
         color: "#4E4E6D",
     }
 }
 const mintInfo = {
-    price: 1.5,
+    price: 1.88,
     totalSupply: 2000,
     toFixed: 2,
     minUnits: 1,
